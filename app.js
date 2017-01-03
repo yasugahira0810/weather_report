a706bf03ae246752d4d62480715873d4285f4243
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2017-01-04 00:02:42 +0900

    Finish implement

--- app.js
+++ app.js
@@ -72,3 +72,18 @@ weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService
   };
 
 }]);
+
+// directive
+weatherApp.directive('weatherReport', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'directives/weatherReport.htm',
+    replace: true,
+    scope: {
+      weatherDay: '=',
+      convertToStandard: '&',
+      convertToDate: '&',
+      dateFormat: '@'
+    }
+  }
+});
